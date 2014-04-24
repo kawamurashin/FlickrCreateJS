1170c76fab99f909d8dd32ecea4410ceb43579e3
kawamurashin <kawamura@mztm.jp>
2014-04-24 18:36:26 +0900

    add php

---
+++ flickr_post.php
@@ -0,0 +1,32 @@
+<?php
+  
+
+$keyword = $_POST['key'];
+
+$limit = 10;
+
+$api_key = '**********************************';
+
+$method   = 'flickr.photos.search';
+
+$text = urlencode($keyword);
+
+$sort = "interestingness-desc";
+
+
+//URLを生成
+$url = 'http://api.flickr.com/services/rest/?'.
+ 'method='.$method.
+ '&api_key='.$api_key.
+ '&text='.$text.
+ '&sort='.$sort.
+ '&per_page='.$limit;
+
+//取得したXMLファイルをパースし、オブジェクトに代入
+$data = file_get_contents($url)
+or die("XMLパースエラー");
+
+echo $data;
+
+
+?>
