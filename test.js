a2849a6a40129e1c69a55345c4d494f41e67a480
kawamurashin <kawamura@mztm.jp>
2014-04-24 18:10:23 +0900

    start git

---
+++ test.js
@@ -0,0 +1,22 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 550,
+	height: 400,
+	fps: 60,
+	color: "#FFFFFF",
+	manifest: []
+};
+
+// stage content:
+(lib.test = function() {
+	this.initialize();
+
+}).prototype = p = new cjs.Container();
+p.nominalBounds = null;
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
